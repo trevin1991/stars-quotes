fdf25c7087357c60f1930156287810339f59f956
Eugene Trofimovich <trofimovich.eugene.projects@gmail.com>
2016-12-28 13:10:54 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,53 @@
+const STARS_LIST = require("./config.js");
+let listNode = document.getElementById("stars"),
+    audio = document.getElementById("audio");
+
+STARS_LIST.forEach((star) => {
+    let starElNode = document.createElement("div"),
+        starTmpl = `<img src="${star.img}" title="${star.name}"/>
+                    <div class="star_name">${star.name}</div>`;
+
+    starElNode.setAttribute("class", "star");
+    starElNode.setAttribute("data-id", star.id);
+    starElNode.innerHTML = starTmpl;
+
+    listNode.appendChild(starElNode);
+});
+
+listNode.addEventListener("click", onStarsListClick);
+
+function onStarsListClick(e) {
+    let node = e.target,
+        isCardClicked = false;
+
+    while(node !== null) {
+        if(node.className === "star") {
+            isCardClicked = true;
+            break;
+        }
+
+        node = node.parentNode;
+    }
+
+    if(!isCardClicked) return;
+
+    let starId = node.getAttribute("data-id");
+    playStarQuote(starId);
+}
+
+function playStarQuote(starId) {
+    if(!starId) return;
+    STARS_LIST.forEach((star) => {
+        if(star.id === starId && star.quotes.length) {
+            let randomQuote = star.quotes[Math.ceil(Math.random() * star.quotes.length) - 1];
+
+            // randomQuote.src
+            // randomQuote.movie
+            // randomQuote.text
+
+            audio.setAttribute("src", randomQuote.src);
+            audio.currentTime = 0;
+            audio.play();
+        }
+    });
+}
---
+++ src/config.js
@@ -0,0 +1,66 @@
+module.exports =
+  [
+    {
+      "id": "s01",
+      "name": "Marlon Brando",
+      "img": "assets/images/marlon-brando.jpg",
+      "quotes": [
+        {
+          "src": "assets/sounds/brando/offer_x.wav",
+          "movie": "The Godfather",
+          "text": "I'm gonna make him an offer he can't refuse."
+        },
+        {
+          "src": "assets/sounds/brando/family_man.wav",
+          "movie": "The Godfather",
+          "text": "Marlon Brando: Ya spend time with your family? Man: Sure, I do. Brando: Good ... because a man who doesn't spend time with his family can never be a real man."
+        },
+        {
+          "src": "assets/sounds/brando/horror.wav",
+          "movie": "Apocalypse Now",
+          "text": "Horror... and moral terror... are your friends."
+        }
+      ]
+    },
+    {
+      "id": "s02",
+      "name": "James Cagney",
+      "img": "assets/images/james-cagney.jpg",
+      "quotes": [
+        {
+          "src": "assets/sounds/cagney/thanks_you.wav",
+          "movie": "Yankee Doodle Dandy",
+          "text": "Ladies and gentlemen... my mother thanks you, my father thanks you, my sister thanks you, and I thank you."
+        }
+      ]
+    },
+    {
+      "id": "s03",
+      "name": "Humphrey Bogart",
+      "img": "assets/images/humphrey-bogart.jpg",
+      "quotes": [
+        {
+          "src": "assets/sounds/bogart/down_to_cases.wav",
+          "movie": "Caine Mutiny",
+          "text": "(Ahem) Now to, uh, get down to cases."
+        },
+        {
+          "src": "assets/sounds/bogart/need_dough.wav",
+          "movie": "Treasure of the Sierra Madre",
+          "text": "I need dough and plenty of it."
+        }
+      ]
+    },
+    {
+      "id": "s04",
+      "name": "Bette Davis",
+      "img": "assets/images/bette-davis.jpg",
+      "quotes": [
+        {
+          "src": "assets/sounds/davis/hate_men.wav",
+          "movie": "All About Eve",
+          "text": "I hate men."
+        }
+      ]
+    }
+  ];
